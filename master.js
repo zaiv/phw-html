f3dba905062ac1237809144252a5cf99f73b6ea5
Pehawe Official <capsindo5@gmail.com>
2018-01-25 00:07:53 +0700

    Update master.js

--- master.js
+++ master.js
@@ -26,7 +26,7 @@ if(x=="1"){xxx="/2017/08/arctic-melt-season-changes-and-arctic.html"}
 if(x=="2"){xxx="/2017/08/us-climate-report-edits-out-highly.html"}
 if(x=="3"){xxx="/2017/08/green-lunacy-fossil-fuel-mandates-to.html"}
 if(x=="4"){xxx="/2017/08/tesla-car-battery-production-releases.html"}
-if(x=="5"){xxx="/2017/08/car-production-fall-hits-uk-industrial.html"}a_to_ve[i].href="https://www.pehawe.ga/p/go-to-url.html"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/08/car-production-fall-hits-uk-industrial.html"}a_to_ve[i].href="https://www.pehawe.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
