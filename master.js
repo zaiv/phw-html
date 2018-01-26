0cffa576399cdee07b0f1f7234d55ab0c5545169
Pehawe Official <capsindo5@gmail.com>
2018-01-26 22:38:53 +0700

    Update master.js

--- master.js
+++ master.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2017/10/ways-of-paying-for-health-insurance.html"}
 if(x=="2"){xxx="/2017/10/traveling-alternative-roads-other.html"}
 if(x=="3"){xxx="/2017/10/health-insurance-limitations.html"}
 if(x=="4"){xxx="/2017/10/for-in-between-times-short-term-health.html"}
-if(x=="5"){xxx="/2017/10/attract-employees-group-health-insurance.html"}a_to_ve[i].href="https://justforsafelink.blogspot.sg/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/10/attract-employees-group-health-insurance.html"}a_to_ve[i].href="https://justforsafelink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
